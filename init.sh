d8fd969762475d2cc34cd7949fbcfe96949a2759
Ahmad Kamal <ahkamal@atlas8.eng.uts.edu.au>
2020-04-14 23:58:12 +1000

    changed to git dir

--- init.sh
+++ init.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 source /home/ahkamal/anaconda3/bin/activate
 conda activate open-mmlab-2
-cd Data/Kaggle_PKU_Baidu/tools
+cd /data/ahkamal/6-DoF_Vehicle_Pose_Estimation_Through_Deep_Learning/tools
 export LD_LIBRARY_PATH=/home/ahkamal/anaconda3/envs/open-mmlab-2/lib/python3.7/site-packages/torch/lib:$LD_LIBRARY_PATH
 
 CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 train_kaggle_pku.py --launcher pytorch
 
 python test_kaggle_pku.py
-
-nvidia-smi
-kill -9 PIDnumber
