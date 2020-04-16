d26369445047627204f1416276747ce1ba49ccef
Ahmad Kamal <ahkamal@atlas6.eng.uts.edu.au>
2020-04-17 09:32:22 +1000

    added mAP to TB plot

--- tools/loss_curveTF.py
+++ tools/loss_curveTF.py
@@ -7,36 +7,43 @@ from datetime import datetime
 import shutil
 
 batch_size = 1
-num_epochs = 43
-num_imgs = 800
-num_iter = 800 # loss intervals for plotting
+num_epochs = 12
+num_imgs = 1600
+num_iter = 1600 # loss intervals for plotting
 
 
 time = '{}'.format(datetime.now().strftime("%b%d-%H-%M"))
-source = '/home/ahkamal/Desktop/losses_log.txt'
+loss_source = '/home/ahkamal/Desktop/losses_log.txt'
+map_source = '/home/ahkamal/Desktop/mAP_log.txt'
 destination = '/data/ahkamal/tf_log/{}/'.format(time)
 
 if not os.path.exists(destination):
 	os.makedirs(destination)
 
-shutil.move(source, destination)
+shutil.move(loss_source, destination)
+shutil.move(map_source, destination)
 
 writer = SummaryWriter(log_dir=destination)
   
 
 loss = []
+mAP = []
 
 with open(destination+'losses_log.txt','r') as file:
 	loss = file.readlines()
+
+with open(destination+'mAP_log.txt','r') as file:
+	mAP = file.readlines()
 
 checkpoint = 0
 l = []
 	
 loss = [float(x.rstrip()) for x in loss]
+mAP = [float(x.rstrip()) for x in mAP]
 
 for i in range(len(loss)):
 
-	if i % num_iter == 0 and i!=0 :
+	if (i+1) % num_iter == 0 and i!= 0 :
 		l.append(sum(loss[checkpoint:i])/(i-checkpoint))
 		checkpoint = i
 	else:
@@ -47,6 +54,7 @@ epoch = np.arange(0, num_epochs, num_epochs/len(l)) # number of successful epoch
 
 for n_iter in range(len(epoch)):
     writer.add_scalar('Loss/train', l[n_iter], n_iter)
+    writer.add_scalar('mAP/val',mAP[n_iter],n_iter+1)
 
 """
 Matplotlib plot
